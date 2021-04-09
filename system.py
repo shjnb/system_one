8e5f70e502b72555e58a9cb75d14ef00eadba909
shjnb <76170758+shjnb@users.noreply.github.com>
2021-04-09 14:33:22 +0800

    Add files via upload

---
+++ system.py
@@ -0,0 +1,12 @@
+# 从app包中导入变量app（它是作为app包成员的变量）
+from app import app, db
+from app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
